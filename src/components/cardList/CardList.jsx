0b6f3374578fb721a860bb98df48b7f1827b6cab
Rajib Chandra Das <prorajib6@gmail.com>
2023-11-17 20:58:24 +0600

    responsive navbar

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import styles from './cardlist.module.css';
+import Pagination from '../pagination/Pagination';
 const CardList = () => {
-  return <div className={styles.container}>CardList</div>;
+  return <div className={styles.container}>
+    <Pagination/>
+  </div>;
 };
 
 export default CardList;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 import styles from './navbar.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+import AuthLinks from '../authLinks/AuthLinks';
+import ThemeToggle from '../themeToggle/ThemeToggle';
 const Navbar = () => {
-  return <div className={styles.container}>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Blogify</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href='/'>Homepage</Link>
+        <Link href='/contact'>Contact</Link>
+        <Link href='/about'>About</Link>
+        <AuthLinks />
+      </div>
+      <div className={styles.social}>
+        <Image src='/facebook.png' alt='facebook' width={24} height={24} />
+        <Image src='/instagram.png' alt='Instagram' width={24} height={24} />
+        <Image src='/tiktok.png' alt='tiktok' width={24} height={24} />
+        <Image src='/youtube.png' alt='Youtube' width={24} height={24} />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
