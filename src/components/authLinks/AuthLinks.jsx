70211dd10ee23f559bc3825fabf2fde6171ce19f
Rajib Chandra Das <prorajib6@gmail.com>
2023-12-04 19:07:07 +0600

    nextAuth configuration

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -2,13 +2,14 @@
 import React, { useState } from 'react';
 import styles from './authLinks.module.css';
 import Link from 'next/link';
+import { signOut, useSession } from 'next-auth/react';
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
-  const status = 'notAuthenticated';
+  const { status } = useSession();
 
   return (
     <>
-      {status === 'notAuthenticated' ? (
+      {status === 'unauthenticated' ? (
         <Link href='/login' className={styles.link}>
           Login
         </Link>
@@ -17,7 +18,9 @@ const AuthLinks = () => {
           <Link href='/write' className={styles.link}>
             Write
           </Link>
-          <span className={styles.link}>Logout</span>
+          <span className={styles.link} onClick={signOut}>
+            Logout
+          </span>
         </>
       )}
       <div className={styles.burger} onClick={() => setOpen(!open)}>
